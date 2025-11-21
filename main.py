476f96f4f71335b7cad8a8b26de76698215b7f77
unknown <brad@compsmart.co.uk>
2025-11-21 19:31:31 +0000

    changed port for islamic version

--- main.py
+++ main.py
@@ -374,4 +374,4 @@ async def websocket_endpoint(websocket: WebSocket):
             pass
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8002, reload=True)
